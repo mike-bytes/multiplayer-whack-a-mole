45396827c3b0e93be5fcc180009d8a0b972342d1
mikeywu <mikeywu@gmail.com>
2026-03-27 14:17:31 -0700

    Put startCountdown as an action for the gameStore since a number of places will use it (e.g wait screen, winnerbanner). Make it into a promise so that caller can await it in case they need to do something after the countdown has finished

--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -6,12 +6,36 @@ export const useGameStore = defineStore('game', {
     players: {},
     winner: null,
     countdown: 0, // seconds
+    countdownInterval: null,
   }),
   actions: {
     setGameState(state) {
       this.activeMoles = state.activeMoles;
       this.players = state.players;
       this.winner = state.winner;
+    },
+    startCountdown(endTime) {
+      // this is a promise so that caller can await on it if desired
+      return new Promise((resolve) => {
+        if (this.countdownInterval) clearInterval(this.countdownInterval);
+        this.countdownInterval = setInterval(() => {
+          const remaining = Math.ceil((endTime - Date.now()) / 1000);
+          this.countdown = Math.max(remaining, 0);
+
+          if (remaining <= 0) {
+            clearInterval(this.countdownInterval);
+            this.countdownInterval = null;
+            this.countdown = 0;
+            resolve();
+          }
+        }, 250);
+      });
+    },
+    stopCountdown() {
+      if (this.countdownInterval) {
+        clearInterval(this.countdownInterval);
+        this.countdownInterval = null;
+      }
     },
   },
 });

dd1ed38e2c1918e29992baae120f1aa8400d0bc6
mikeywu <mikeywu@gmail.com>
2026-03-27 14:15:27 -0700

    Component for animated dots which are used in mutliple places to show wait progress

---
+++ src/components/AnimatedDots.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="dots"></span>
+</template>
+
+<script>
+export default {
+  name: 'Dots',
+};
+</script>
+
+<style scoped>
+.dots::after {
+  content: '';
+  display: inline-block;
+  width: 1em;
+  text-align: left;
+  animation: dotPulse 2s steps(3, end) infinite;
+}
+
+@keyframes dotPulse {
+  0% {
+    content: '';
+  }
+  33% {
+    content: '.';
+  }
+  66% {
+    content: '..';
+  }
+  100% {
+    content: '...';
+  }
+}
+</style>
